a86a85016eed840fa275c1b3b005bc68653de7e7
wangchangxian <changxian520007@126.com>
2016-06-02 15:52:14 +0800

    f

---
+++ conf/database.php
@@ -0,0 +1,10 @@
+<?php
+  return array(
+	'dsn' => 'mysql:host=173.254.246.105;dbname=phpwind;port=3306',
+	'user' => 'root',
+	'pwd' => 'zoujunrong123019Z',
+	'charset' => 'utf8',
+	'tableprefix' => 'pw_',
+	'engine' => 'MyISAM',
+);
+?>
---
+++ data/log/admin_log.php
@@ -0,0 +1,59 @@
+<?php die;?>
+|admin|Login Successful|127.0.0.1|1464846690|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=18b8caa50a737875|
+|admin|Login Successful|127.0.0.1|1464846691|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=18b8caa50a737875|
+|admin|bbs/article/run|127.0.0.1|1464846713|/admin.php?m=bbs&c=article||
+|admin|tag/manage/run|127.0.0.1|1464846723|/admin.php?m=tag&c=manage||
+|admin|message/manage/run|127.0.0.1|1464846750|/admin.php?m=message&c=manage||
+|admin|report/manage/run|127.0.0.1|1464846753|/admin.php?m=report&c=manage||
+|admin|message/manage/run|127.0.0.1|1464846754|/admin.php?m=message&c=manage||
+|admin|bbs/configbbs/run|127.0.0.1|1464846762|/admin.php?m=bbs&c=configbbs||
+|admin|bbs/setforum/run|127.0.0.1|1464846763|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464846777|/admin.php?fid=1&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1464846792|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=1, forumname=官方区, manager=, vieworder=0, descrip=, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=50, threadperpage=20, newtime=60, threadorderby=0, password=***, csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464846795|/admin.php?fid=1&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1464846801|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464846817|/admin.php?fid=2&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1464846829|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=2, forumname=论坛公告, manager=, vieworder=0, parentid=1, descrip=, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=50, threadperpage=20, readperpage=15, newtime=60, threadorderby=0, minlengthofcontent=3, locktime=, edittime=, allowtype=array(0=default)typeorder=array(default=0, poll=)allowhide=1, allowsell=1, contentcheck=0, ifthumb=0, thumbwidth=, thumbheight=, water=0, waterimg=mark.gif, topic_type=0, force_topic_type=0, topic_type_display=1, password=***, creditset=array(post_topic=array(limit=, credit=array(1=, 2=, 3=))delete_topic=array(limit=, credit=array(1=, 2=, 3=))post_reply=array(limit=, credit=array(1=, 2=, 3=))delete_reply=array(limit=, credit=array(1=, 2=, 3=))digest_topic=array(limit=, credit=array(1=, 2=, 3=))remove_digest=array(limit=, credit=array(1=, 2=, 3=))push_thread=array(limit=, credit=array(1=, 2=, 3=))upload_att=array(limit=, credit=array(1=, 2=, 3=))download_att=array(limit=, credit=array(1=, 2=, 3=))belike=array(limit=, credit=array(1=, 2=, 3=)))csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464846831|/admin.php?fid=2&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1464846836|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/dorun|127.0.0.1|1464846852|/admin.php?m=bbs&c=setforum&a=dorun|vieworder=array(1=0, 2=0)manager=array(1=, 2=)new_vieworder=array(0=array(0=))new_forumname=array(0=array(0=新手攻略))tempid=array(0=array(0=2))new_manager=array(0=array(0=))csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/run|127.0.0.1|1464846855|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464846868|/admin.php?fid=3&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1464846880|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=3, forumname=产品区, manager=, vieworder=0, descrip=, style=, across=0, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=, threadperpage=, newtime=0, threadorderby=0, password=***, csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464846884|/admin.php?fid=3&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1464846886|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/dorun|127.0.0.1|1464846936|/admin.php?m=bbs&c=setforum&a=dorun|vieworder=array(1=0, 2=0, 3=0)manager=array(1=, 2=, 3=)new_vieworder=array(1=array(0=, 1=, 2=))new_forumname=array(1=array(0=官方活动, 1=官方动态, 2=新手攻略))tempid=array(1=array(0=4, 1=3, 2=2))new_manager=array(1=array(0=, 1=, 2=))csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/run|127.0.0.1|1464846939|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/dorun|127.0.0.1|1464846972|/admin.php?m=bbs&c=setforum&a=dorun|vieworder=array(1=0, 2=1, 4=4, 5=3, 6=2, 3=0)manager=array(1=, 2=, 4=, 5=, 6=, 3=)csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/run|127.0.0.1|1464846975|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/dorun|127.0.0.1|1464847035|/admin.php?m=bbs&c=setforum&a=dorun|vieworder=array(1=1, 2=1, 6=2, 5=3, 4=4, 3=2)manager=array(1=, 2=, 6=, 5=, 4=, 3=)new_vieworder=array(3=array(0=1, 1=2, 2=3, 3=4))new_forumname=array(3=array(0=H3 知识窗, 1=产品需求, 2=实例方案, 3=BUG反馈))tempid=array(3=array(0=5, 1=4, 2=3, 3=2))new_manager=array(3=array(0=, 1=, 2=, 3=))csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/run|127.0.0.1|1464847037|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/dorun|127.0.0.1|1464847097|/admin.php?m=bbs&c=setforum&a=dorun|vieworder=array(1=1, 2=1, 6=2, 5=3, 4=4, 3=2, 7=1, 8=2, 9=3, 10=4)manager=array(1=, 2=, 6=, 5=, 4=, 3=, 7=, 8=, 9=, 10=)new_vieworder=array(0=array(0=3))new_forumname=array(0=array(0=综合区))tempid=array(0=array(0=2)2=array(0=6, 1=5, 2=4, 3=3))new_manager=array(0=array(0=))temp_vieworder=array(2=array(0=1, 1=2, 2=3, 3=4))temp_forumname=array(2=array(0=招聘求职, 1=谈天说地, 2=论坛事务, 3=同城会))temp_manager=array(2=array(0=, 1=, 2=, 3=))csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/run|127.0.0.1|1464847101|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464847114|/admin.php?fid=1&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/edit|127.0.0.1|1464847137|/admin.php?fid=3&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1464847145|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=3, forumname=产品区, manager=, vieworder=2, descrip=, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=, threadperpage=, newtime=0, threadorderby=0, password=***, csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464847148|/admin.php?fid=3&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1464847151|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464847154|/admin.php?fid=11&m=bbs&c=setforum&a=edit||
+|admin|bbs/setforum/doedit|127.0.0.1|1464847159|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=11, forumname=综合区, manager=, vieworder=3, descrip=, style=, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=, threadperpage=, newtime=0, threadorderby=0, password=***, csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464847162|/admin.php?fid=11&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|bbs/setforum/run|127.0.0.1|1464847184|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464847188|/admin.php?fid=2&m=bbs&c=setforum&a=edit||
+|admin|bbs/setbbs/run|127.0.0.1|1464847614|/admin.php?m=bbs&c=setbbs||
+|admin|bbs/setforum/run|127.0.0.1|1464847618|/admin.php?m=bbs&c=setforum||
+|admin|bbs/setforum/edit|127.0.0.1|1464847671|/admin.php?fid=1&m=bbs&c=setforum&a=edit||
+|admin|default/custom/run|127.0.0.1|1464847708|/admin.php?c=custom||
+|admin|bbs/setbbs/read|127.0.0.1|1464847736|/admin.php?m=bbs&c=setbbs&a=read||
+|admin|bbs/setbbs/thread|127.0.0.1|1464847747|/admin.php?m=bbs&c=setbbs&a=thread||
+|admin|bbs/setforum/doedit|127.0.0.1|1464849199|/admin.php?_json=1&m=bbs&c=setforum&a=doedit|fid=1, forumname=官方区, manager=, vieworder=1, descrip=, style=default, across=2, isshow=1, jumpurl=, icon=, logo=, forumdomain=, seo=array(title=, keywords=, description=)numofthreadtitle=50, threadperpage=20, newtime=60, threadorderby=0, password=***, csrf_token=18b8caa50a737875|
+|admin|bbs/setforum/edit|127.0.0.1|1464849203|/admin.php?fid=1&m=bbs&c=setforum&a=edit&tab=nav_jb||
+|admin|Login Successful|10.10.92.232|1464852507|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=1c306f45b0de2ff1|
+|admin|config/config/run|10.10.92.232|1464852517|/admin.php?m=config&c=config||
+|admin|nav/nav/run|10.10.92.232|1464852522|/admin.php?m=nav&c=nav||
+|admin|nav/nav/dorun|10.10.92.232|1464852539|/admin.php?m=nav&c=nav&a=dorun|data=array(1=array(navid=1, orderid=1, name=首页, link=index.php)2=array(navid=2, orderid=2, name=论坛, link=index.php?m=bbs)3=array(navid=3, orderid=3, name=版块, link=index.php?m=bbs&c=forumlist)4=array(navid=4, orderid=4, name=喜欢, link=index.php?m=like&c=like)6=array(navid=6, orderid=5, name=话题, link=index.php?m=tag)7=array(navid=7, orderid=6, name=应用, link=index.php?m=appcenter)5=array(navid=5, orderid=7, name=云平台, link=http://open.phpwind.com))home=2, navtype=main, csrf_token=1c306f45b0de2ff1|
+|admin|nav/nav/run|10.10.92.232|1464852542|/admin.php?m=nav&c=nav||
+|admin|nav/nav/dorun|10.10.92.232|1464852595|/admin.php?m=nav&c=nav&a=dorun|data=array(1=array(navid=1, orderid=1, name=首页, link=index.php)2=array(navid=2, orderid=2, name=论坛, link=index.php?m=bbs, isshow=1)3=array(navid=3, orderid=3, name=版块, link=index.php?m=bbs&c=forumlist)4=array(navid=4, orderid=4, name=喜欢, link=index.php?m=like&c=like)6=array(navid=6, orderid=5, name=话题, link=index.php?m=tag)7=array(navid=7, orderid=6, name=应用, link=index.php?m=appcenter)5=array(navid=5, orderid=7, name=云平台, link=http://open.phpwind.com))home=2, newdata=array(root_2=array(orderid=8, name=兑换商城, tempid=temp_root_2, link=, isshow=1)root_3=array(orderid=9, name=官方网站, tempid=temp_root_3, link=, isshow=1))navtype=main, csrf_token=1c306f45b0de2ff1|
+|admin|nav/nav/run|10.10.92.232|1464852599|/admin.php?m=nav&c=nav||
+|admin|nav/nav/dorun|10.10.92.232|1464852628|/admin.php?m=nav&c=nav&a=dorun|data=array(1=array(navid=1, orderid=1, name=首页, link=index.php)2=array(navid=2, orderid=2, name=论坛, link=index.php?m=bbs, isshow=1)3=array(navid=3, orderid=3, name=版块, link=index.php?m=bbs&c=forumlist)4=array(navid=4, orderid=4, name=喜欢, link=index.php?m=like&c=like)6=array(navid=6, orderid=5, name=话题, link=index.php?m=tag)7=array(navid=7, orderid=6, name=应用, link=index.php?m=appcenter)5=array(navid=5, orderid=7, name=云平台, link=http://open.phpwind.com)19=array(navid=19, orderid=8, name=兑换商城, link=http://www.shengli.com, isshow=1)20=array(navid=20, orderid=9, name=官方网站, link=http://www.shengli.com, isshow=1))home=2, navtype=main, csrf_token=1c306f45b0de2ff1|
+|admin|nav/nav/run|10.10.92.232|1464852631|/admin.php?m=nav&c=nav||
